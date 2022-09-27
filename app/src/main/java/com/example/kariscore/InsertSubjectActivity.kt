d73fa8102a467e38a023eb73521538ec9972215d
Archann <chanika.k@kkumail.com>
2022-09-27 23:13:56 +0700

    commit 27/09/2022 23:15

--- app/src/main/java/com/example/kariscore/InsertSubjectActivity.kt
+++ app/src/main/java/com/example/kariscore/InsertSubjectActivity.kt
@@ -3,9 +3,9 @@ package com.example.kariscore
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class EditDeleteStudentActivity : AppCompatActivity() {
+class InsertSubjectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_delete_student)
+        setContentView(R.layout.activity_insert_subject)
     }
 }
