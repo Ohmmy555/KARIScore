46cb85d12ffa7ddc2d05d7e303bae2645da8c2b7
ifair-dev <wuttichai.kh@kkumail.com>
2022-09-27 23:09:18 +0700

    commit 27/09/2022 23:10

--- app/src/main/java/com/example/kariscore/Subject.kt
+++ app/src/main/java/com/example/kariscore/Subject.kt
@@ -1,4 +1,3 @@
 package com.example.kariscore
 
-data class Subject(val id:String,val name:String,val year:String,val team:String) {
-}
+data class Subject(val id:String,val name:String,val year:String,val team:String) {}
